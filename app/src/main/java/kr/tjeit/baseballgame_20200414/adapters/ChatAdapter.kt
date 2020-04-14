4f310b3c9b09a9af12ce3066c4532f281db58fc1
cho881020 <kj_cho@idealidea.co.kr>
2020-04-14 21:56:17 +0900

    채팅 내용 뿌려주기

--- app/src/main/java/kr/tjeit/baseballgame_20200414/adapters/ChatAdapter.kt
+++ app/src/main/java/kr/tjeit/baseballgame_20200414/adapters/ChatAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.TextView
 import kr.tjeit.baseballgame_20200414.R
 import kr.tjeit.baseballgame_20200414.datas.Chat
 
@@ -22,6 +24,26 @@ class ChatAdapter(context:Context, resId:Int, list:ArrayList<Chat>) : ArrayAdapt
             tempRow = inf.inflate(R.layout.chat_list_item, null)
         }
         val row = tempRow!!
+
+        val computerLayout = row.findViewById<LinearLayout>(R.id.computerLayout)
+        val userLayout = row.findViewById<LinearLayout>(R.id.userLayout)
+        val computerTxt = row.findViewById<TextView>(R.id.computerTxt)
+        val userTxt = row.findViewById<TextView>(R.id.userTxt)
+
+        val data = mList.get(position)
+
+        if (data.speaker == "COMPUTER") {
+            computerLayout.visibility = View.VISIBLE
+            userLayout.visibility = View.GONE
+
+            computerTxt.text = data.content
+        }
+        else {
+            computerLayout.visibility = View.GONE
+            userLayout.visibility = View.VISIBLE
+
+            userTxt.text = data.content
+        }
 
 
         return row
