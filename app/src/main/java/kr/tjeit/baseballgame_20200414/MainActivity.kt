e835c4bcd0f30a3ec5c19a882bf06c6498bd2701
cho881020 <kj_cho@idealidea.co.kr>
2020-04-14 21:51:33 +0900

    어댑터 / 리스트뷰 연결

--- app/src/main/java/kr/tjeit/baseballgame_20200414/MainActivity.kt
+++ app/src/main/java/kr/tjeit/baseballgame_20200414/MainActivity.kt
@@ -2,8 +2,14 @@ package kr.tjeit.baseballgame_20200414
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.tjeit.baseballgame_20200414.adapters.ChatAdapter
+import kr.tjeit.baseballgame_20200414.datas.Chat
 
 class MainActivity : BaseActivity() {
+
+    val chatings = ArrayList<Chat>()
+    var mChatAdapter:ChatAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +24,13 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        chatings.add(Chat("숫자 야구게임에 오신걸 환영합니다.", "COMPUTER"))
+        chatings.add(Chat("세자리 숫자를 맞춰주세요.", "COMPUTER"))
+        chatings.add(Chat("중복된 숫자는 없고, 0도 사용되지 않습니다.", "COMPUTER"))
+
+        mChatAdapter = ChatAdapter(mContext, R.layout.chat_list_item, chatings)
+        chatListView.adapter = mChatAdapter
 
     }
 
