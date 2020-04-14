8f817b4dea620116b74a1f528ce969e6bf15f375
cho881020 <kj_cho@idealidea.co.kr>
2020-04-14 22:17:00 +0900

    컴퓨터 문제 출제 로직

--- app/src/main/java/kr/tjeit/baseballgame_20200414/MainActivity.kt
+++ app/src/main/java/kr/tjeit/baseballgame_20200414/MainActivity.kt
@@ -2,11 +2,15 @@ package kr.tjeit.baseballgame_20200414
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import kr.tjeit.baseballgame_20200414.adapters.ChatAdapter
 import kr.tjeit.baseballgame_20200414.datas.Chat
 
 class MainActivity : BaseActivity() {
+
+//    문제 숫자 세자리가 담길 ArrayList
+    val computerNumbers = ArrayList<Int>()
 
     val chatings = ArrayList<Chat>()
     var mChatAdapter:ChatAdapter? = null
@@ -16,8 +20,44 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
         setupEvents()
         setValues()
+        makeComputerNumber()
     }
 
+//    컴퓨터가 문제 출제하는 함수
+    fun makeComputerNumber() {
+
+        for (i in 0..2) {
+
+            while (true) {
+
+//            1. 1~9의 숫자만 사용 => 랜덤으로 생성되어야함.
+//            2. 이미 뽑힌 숫자는 사용하면 안됨. (중복허용 X)
+//      1 <= Math.random()*9+1 < 10
+                val tempNum = (Math.random()*9+1).toInt()
+
+//        중복검사 => 컴퓨터 숫자 배열안에 이 임시번호가 같은게 있다면?
+                var isDuplOk = true
+
+                for (cpNum in computerNumbers) {
+                    if (tempNum == cpNum) {
+                        isDuplOk = false
+                    }
+                }
+
+                if (isDuplOk) {
+                    computerNumbers.add(tempNum)
+                    break
+                }
+            }
+
+        }
+
+        for (num in computerNumbers) {
+            Log.d("출제번호", num.toString())
+        }
+
+
+    }
 
     override fun setupEvents() {
 
